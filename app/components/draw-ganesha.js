9652470bdb391b5aeab2f8ae84f10843a146703f
Manjunath, Vishwaas <Vishwaas_Manjunath@gallup.com>
2017-05-20 18:24:57 -0500

    Adding contact page

--- app/components/draw-ganesha.js
+++ app/components/draw-ganesha.js
@@ -5,14 +5,26 @@ export default Ember.Component.extend({
 
   titleText: '',
 
+  animatedText: '',
+
+  animationComplete: false,
+
   didInsertElement() {
     this._super(...arguments);
     let _this = this;
+    let titleText = this.get('titleText') || '';
     const TEXT_DURATION_DISPLAY = 200;
-    this.get('titleText').split('').reduce((accumulator, currentValue, currentIndex) => {
+    titleText.split('').reduce((accumulator, currentValue, currentIndex) => {
       let newValue = accumulator + currentValue;
       setTimeout(() => {
-        _this.$('.page-title').text(newValue);
+        //_this.$('.page-title').text(newValue);
+        _this.set('animatedText', newValue);
+        if (currentIndex === titleText.length - 1) {
+          setTimeout(() => {
+            _this.set('animationComplete', true);
+          }, TEXT_DURATION_DISPLAY);
+
+        }
       }, ((currentIndex + 1) * TEXT_DURATION_DISPLAY));
 
       return newValue;
--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('aboutus');
+  this.route('contact-us');
 });
 
 export default Router;
